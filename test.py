26bae867d0d9e5a2d0706e11ac236242fafb202d
Ubuntu <arvindvs@cs224n-vm1.lc10qw5q0fre5fubfqyek3yayf.bx.internal.cloudapp.net>
2019-03-07 20:51:21 +0000

    Updated parameters passed into model in test.py

--- test.py
+++ test.py
@@ -41,11 +41,13 @@ def main(args):
     # Get embeddings
     log.info('Loading embeddings...')
     word_vectors = util.torch_from_json(args.word_emb_file)
+    char_vectors = util.torch_from_json(args.char_emb_file)
 
     # Get model
     log.info('Building model...')
     model = BiDAFTransformer(word_vectors=word_vectors,
-                  hidden_size=args.hidden_size)
+                             char_vectors=char_vectors,
+                             hidden_size=args.hidden_size)
     model = nn.DataParallel(model, gpu_ids)
     log.info('Loading checkpoint from {}...'.format(args.load_path))
     model = util.load_model(model, args.load_path, gpu_ids, return_step=False)
@@ -78,8 +80,11 @@ def main(args):
             qw_idxs = qw_idxs.to(device)
             batch_size = cw_idxs.size(0)
 
+            cc_idxs = cc_idxs.to(device)
+            qc_idxs = qc_idxs.to(device)
+
             # Forward
-            log_p1, log_p2 = model(cw_idxs, qw_idxs)
+            log_p1, log_p2 = model(cw_idxs, qw_idxs, cc_idxs, qc_idxs)
             y1, y2 = y1.to(device), y2.to(device)
             loss = F.nll_loss(log_p1, y1) + F.nll_loss(log_p2, y2)
             nll_meter.update(loss.item(), batch_size)
